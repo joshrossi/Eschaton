df6c6973587c0672a7a011e953d8c576b4083545
Joshua Rossi <joshrossi@yahoo.com>
2011-12-27 00:26:28 -0500

    finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Eschaton::Application.routes.draw do
+  get "users/new"
+
+  match '/signup',  :to => 'users#new'
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+
+  root :to => 'pages#home'
+  
   get "pages/home"
 
   get "pages/contact"
@@ -54,7 +63,6 @@ Eschaton::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
