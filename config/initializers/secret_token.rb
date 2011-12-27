0ba0609d0b4df53d89d1bb018c557f79113e8dd1
Joshua Rossi <joshrossi@yahoo.com>
2011-12-26 22:07:47 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eschaton::Application.config.secret_token = 'e514edbdc9074977afea0e963c85e28b2e1ccdb93d31adbd9abee72a22e3b1eda3c5f2db98592c20d071e8c7ec4d3c704919a517f3d8d16fcdd271c436696c09'
